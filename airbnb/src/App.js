161237b8af1bbce84372c756165b072c9adbc26a
Divya Unnikrishnan <unnikrishnan.d@northeastern.edu>
2023-05-12 02:46:02 -0400

    Hero added

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "./Components/NavBar.js"
+import Hero from "./Components/Hero.js";
 
 export default function App() {
     return (
         <div>
             <Navbar />
+            <Hero />
         </div>
     )
 }
--- airbnb/src/Components/Hero.js
+++ airbnb/src/Components/Hero.js
@@ -1,8 +1,13 @@
 import React from "react";
+import grid from "../images/airbnb-grid.png"
 
 function Hero(){
     return(
-        <></>
+        <section className="hero">
+            <img src={grid} alt="" className="hero--photo"></img>
+            <h1 className="hero--header">Online Experience</h1>
+            <p className="hero--text">Join unique activites led by one-of-a-kind hosts-all without leaving home.</p>
+        </section>
     )
 }
 
