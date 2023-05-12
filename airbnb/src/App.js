f236d856d1d2c717887e354c3797179f094474a8
Divya Unnikrishnan <unnikrishnan.d@northeastern.edu>
2023-05-12 00:58:18 -0400

    Inital Scaffolding

---
+++ airbnb/src/App.js
@@ -0,0 +1,16 @@
+import Card from './Components/Card.js';
+import Hero from './Components/Hero.js';
+import NavBar from './Components/NavBar.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Card />
+      <Hero /> */}
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ airbnb/src/Components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function NavBar(){
+    return(
+        <nav>
+            <img src="../images/airbnb-logo.png" alt=""></img>
+        </nav>
+    )
+}
+
+export default NavBar;
