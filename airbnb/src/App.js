d3ce5e531753f8d24a3b59cbced6f0346ea73d1a
Divya Unnikrishnan <unnikrishnan.d@northeastern.edu>
2023-05-14 16:54:31 -0400

    Card component added

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -1,12 +1,14 @@
 import React from "react"
 import Navbar from "./Components/NavBar.js"
 import Hero from "./Components/Hero.js";
+import Card from "./Components/Card.js";
 
 export default function App() {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Card />
         </div>
     )
 }
--- airbnb/src/Components/Card.js
+++ airbnb/src/Components/Card.js
@@ -1,8 +1,18 @@
 import React from "react";
-
+import jim from "../images/Jim-halpert.jpg";
 function Card(){
     return(
-        <></>
+        <div className="card">
+            <img src={jim} alt=""></img>
+            <div className="card--stats">
+                <img src="" alt=""></img>
+                <span>5.0</span>
+                <span>(6) </span>
+                <span>USA</span>
+            </div>
+            <p>Life Lessons with Jim Halpert</p>
+            <p>From $136 / person</p>
+        </div>
     )
 }
 
