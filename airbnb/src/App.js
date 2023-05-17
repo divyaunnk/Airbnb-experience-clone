2173b795a94072bef96c6ab752fd304f5676774b
niulpanvalkar <niul.omega@gmail.com>
2023-05-16 22:57:51 -0400

    Changed props

--- airbnb/src/App.js
+++ airbnb/src/App.js
@@ -2,13 +2,32 @@ import React from "react"
 import Navbar from "./Components/NavBar.js"
 import Hero from "./Components/Hero.js";
 import Card from "./Components/Card.js";
+import data from "../src/data/experiencesData.js";
 
 export default function App() {
+
+    const cards = data.map((element) => {
+      return (
+        <Card
+          key={element.id}
+          element={element}
+          // img={element.coverImg}
+          // rating={element.stats.rating}
+          // reviewCount={element.stats.reviewCount}
+          // country={element.country}
+          // title={element.title}
+          // price={element.price}
+          // openSpots={element.openSpots}
+          // location={element.location}
+        />
+      );
+    });
     return (
         <div>
             <Navbar />
             <Hero />
-            <Card />
+            <div className="cards">{cards}</div>
+            
         </div>
     )
 }
--- airbnb/src/Components/Card.js
+++ airbnb/src/Components/Card.js
@@ -1,18 +1,28 @@
 import React from "react";
-import jim from "../images/Jim-halpert.jpg";
-function Card(){
+
+
+function Card(props){
+
+    let badgeText;
+    if(props.element.openSpots === 0) {
+        badgeText = "SOLD"
+    } else if (props.element.location === "Online") {
+        badgeText = "ONLINE";
+    }
+     
     return(
         <div className="card">
-            <img src={jim} alt=""></img>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img src={`../images/${props.element.coverImg}`} alt="Jim Halper cover picture" className="card--image"></img>
             <div className="card--stats">
-                <img src="" alt=""></img>
-                <span>5.0</span>
-                <span>(6) </span>
-                <span>USA</span>
+                <img src="../images/star-solid.svg" className="card--star" alt="Star icon"/>
+                <span>&nbsp;{props.element.stats.rating}&nbsp;</span>
+                <span>({props.element.stats.reviewCount})&nbsp;</span>
+                <span>{props.element.country}&nbsp;</span>
             </div>
-            <p>Life Lessons with Jim Halpert</p>
-            <p>From $136 / person</p>
-        </div>
+            <p>{props.element.title}</p> 
+            <p>From ${props.element.price}/ person</p>
+        </div> 
     )
 }
 
